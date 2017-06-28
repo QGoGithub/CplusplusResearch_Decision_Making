17c7d032ddfbcb6f283afc8ed0c79358a9e2e7c9
Hexal <hexalations@gmail.com>
2017-06-28 11:55:41 +0100

    Find factorial of a number using recursion
    Find factorial using recursion

---
+++ Find_factorial_Recursion.cpp
@@ -0,0 +1,34 @@
+/*
+ * C++ Program to Find Factorial of a Number using Recursion
+ */
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+#define ll long long
+using namespace std;
+/*
+ * Find Factorial of a Number using Recursion
+ */
+ll fact_recur(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * fact_recur(n - 1);
+}
+/*
+ * Main
+ */
+int main()
+{
+    int n;
+    while (1)
+    {
+        cout<<"Enter interger to compute factorial(0 to exit): ";
+        cin>>n;
+        if (n == 0)
+            break;
+        cout<<fact_recur(n)<<endl;
+    }
+    return 0;
+}
